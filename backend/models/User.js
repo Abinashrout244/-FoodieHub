f374c56b6fea42d0aac2307eed59503fde8528ab
Abinashrout244 <routabinash3775@gmail.com>
2026-03-26 07:46:32 +0530

    fix:try-catch

--- backend/models/User.js
+++ backend/models/User.js
@@ -28,10 +28,14 @@ const userSchema = new mongoose.Schema({
 
 // Hash password before saving
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+ try{
+ if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
+ }catch(err){
+  next(err)
+ }
 });
 
 // Compare password method
